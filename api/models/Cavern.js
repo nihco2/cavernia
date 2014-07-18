88221309967486001607eeb666eeeed0b436cc2d
Nicolas Martin <nihco2@gmail.com>
2014-07-18 21:31:45 +0200

    init sails app

---
+++ api/models/Cavern.js
@@ -0,0 +1,24 @@
+/**
+ * Cavern
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+    "nom": "STRING",
+    "departement": "STRING",
+    "commune": "STRING",
+    "coords": "STRING",
+    "description": "STRING",
+    "acces": "STRING",
+    "topographie": "STRING",
+    "developpement": "STRING",
+    "denivele": "STRING",
+    "inventeur": "STRING"
+  }
+
+};
