419ac29cb2464426da9ce2e8ea422663d29efd13
Nicolas Martin <nihco2@gmail.com>
2014-07-26 16:56:34 +0200

    Improve converter

--- assets/js/app/mixins/Converter.js
+++ assets/js/app/mixins/Converter.js
@@ -5,11 +5,11 @@ define([
     convert: function(latitude, longitude) {
       var self = this;
       return new Ember.RSVP.Promise(function(resolve, reject) {
-        var r = Math.sqrt(Math.pow(latitude - 700000.0, 2) + Math.pow(longitude - 12655612.05, 2)),
-          theta = Math.atan((latitude - 700000.0) / (12655612.05 - longitude)),
-          lambda = 0.0523598776 + theta / 0.725607765,
-          isomlat = -(1 / 0.725607765) * Math.log(Math.abs(r / 11754255.426)),
-          phi = self.computeLatitude(isomlat, 0.08248325676, 0.00000000001),
+        var r = Math.sqrt(Math.pow(latitude - 700000.0, 2) + Math.pow(longitude - 12655612.04987618, 2)),
+          theta = Math.atan((latitude - 700000.0) / (12655612.04987618 - longitude)),
+          lambda = 0.0523598776 + theta / 0.725607765053268,
+          isomlat = -(1 / 0.725607765053268) * Math.log(Math.abs(r / 11754255.426095668)),
+          phi = self.computeLatitude(isomlat, 0.08181919104, 0.00000000001),
           coords = {
             latitude: phi * 180 / Math.PI,
             longitude: lambda * 180 / Math.PI
